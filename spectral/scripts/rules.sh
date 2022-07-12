90224ce65fe8192659ca9e129377416c34e131d5
dimakis <dimitri.saridakis@gmail.com>
2022-07-12 18:09:17 +0100

    ci: addition of more apis to the CI

--- spectral/scripts/rules.sh
+++ spectral/scripts/rules.sh
@@ -7,7 +7,19 @@
 
 echo "fetching kas-fleet-manager from main" 
 wget -P .openapi https://raw.githubusercontent.com/redhat-developer/app-services-sdk-js/main/.openapi/kas-fleet-manager.yaml
-
+echo "fetching connector_mgmgt from main" 
+wget -P .openapi https://github.com/redhat-developer/app-services-sdk-js/blob/main/.openapi/connector_mgmt.yaml
+echo "fetching srs from main" 
+wget -P .openapi https://github.com/redhat-developer/app-services-sdk-js/blob/main/.openapi/srs-fleet-manager.json
+echo "fetching smartevents from main" 
+wget -P .openapi https://github.com/redhat-developer/app-services-sdk-js/blob/main/.openapi/smartevents-mgmt.yaml
+echo "fetching kafka-admin from main" 
+wget -P .openapi https://github.com/redhat-developer/app-services-sdk-js/blob/main/.openapi/kafka-admin-rest.yaml
 ## TODO add more apis
 
+## linting the fetched apis
+yarn spectral lint .openapi/connector_mgmt.yaml -v -r ./examples/.spectral-local.yaml
 yarn spectral lint .openapi/kas-fleet-manager.yaml -v -r ./examples/.spectral-local.yaml
+yarn spectral lint .openapi/srs-fleet-manager.json -v -r ./examples/.spectral-local.yaml
+yarn spectral lint .openapi/smartevents-mgmt.yaml -v -r ./examples/.spectral-local.yaml
+yarn spectral lint .openapi/kafka-admin-rest.yaml -v -r ./examples/.spectral-local.yaml
