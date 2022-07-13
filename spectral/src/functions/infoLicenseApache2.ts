b1a14040057930d3fe5332f620d6eb516b2dda15
dimakis <dimitri.saridakis@gmail.com>
2022-07-13 11:54:00 +0100

    fix: fixes a bug in path for infoLicense func

--- spectral/src/functions/infoLicenseApache2.ts
+++ spectral/src/functions/infoLicenseApache2.ts
@@ -15,20 +15,22 @@ export default (targetVal: any, _: any, context: any): IFunctionResult[] => {
 
 	const results: IFunctionResult[] = [];
 	const expectName = 'Apache 2.0'
-	p = context.path[1];
+	if (context.path[1] != undefined) {
+		p = context.path[1];
+	}
 	if (targetVal?.name != expectName) {
 		results.push({
 			message: '`name` must be "' + expectName + '"',
-			path: [...p, 'license']
+			path: p
 		})
-	}	
+	}
 	const expectUrl = 'https://www.apache.org/licenses/LICENSE-2.0';
 	if (targetVal?.url !== expectUrl) {
 		results.push({
 			message: '`url` must be "' + expectUrl + '" not "' + targetVal?.url + '"',
-			path: [...p, 'license']
+			path: p
 		})
-	}																	
+	}
 
 	return results
 }
